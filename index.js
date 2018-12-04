f328c56a667bb5a2d5d71d6b5af2989524fa2a74
syun415 <urassyun@yahoo.co.jp>
2018-12-04 23:24:37 +0900

    add function of send ir

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
 var express = require("express");
 var app = express();
+var exec = require('child_process').exec;
 
 /* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
 var server = app.listen(process.env.PORT || '3000', function(){
@@ -33,5 +34,30 @@ socket.on('connect', () => {
     socket.on('messaged', (obj) => {
         // io.emit('chat message', msg);
         console.log("message:"+obj.message);
+        var command ="";
+        switch(obj.message){
+            case "power":
+                command ="irsend SEND_ONCE fan PWR";
+                break;
+            case "whigh":
+                command ="irsend SEND_ONCE fan WHIGH";
+                break;
+            case "wlow":
+                command ="irsend SEND_ONCE fan WLOW";
+                break;
+        }
+        if(command !=""){
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    console.error('[ERROR] ${error}');
+                    return;
+                }
+                //console.log(`stdout: ${stdout}`);
+                //console.log(`stderr: ${stderr}`);
+            });
+        }
+       
+
+
     });
 });
