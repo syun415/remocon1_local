cb354ba1eafb2f7ffc57af9627c85222fa8bb624
syun415 <urassyun@yahoo.co.jp>
2018-12-05 13:43:55 +0900

    add switch

--- index.js
+++ index.js
@@ -36,14 +36,35 @@ socket.on('connect', () => {
         console.log("message:"+obj.message);
         var command ="";
         switch(obj.message){
-            case "power":
+            case "fpwr":
                 command ="irsend SEND_ONCE fan PWR";
+                break;
+            case "wlow":
+                command ="irsend SEND_ONCE fan WLOW";
+                break;
+            case "wmid":
+                command ="irsend SEND_ONCE fan WMID";
                 break;
             case "whigh":
                 command ="irsend SEND_ONCE fan WHIGH";
                 break;
-            case "wlow":
-                command ="irsend SEND_ONCE fan WLOW";
+            case "rlow":
+                command ="irsend SEND_ONCE fan RLOW";
+                break;
+            case "rmid":
+                command ="irsend SEND_ONCE fan RMID";
+                break;
+            case "rhigh":
+                command ="irsend SEND_ONCE fan RHIGH";
+                break;
+            case "tim1":
+                command ="irsend SEND_ONCE fan TIM1";
+                break;
+            case "tim2":
+                command ="irsend SEND_ONCE fan TIM2";
+                break;
+            case "tim4":
+                command ="irsend SEND_ONCE fan TIM4";
                 break;
         }
         if(command !=""){
